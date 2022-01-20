6625c8df556e163dc23dbf68961ac933f8ca29cb
Kamil Tyugaev <samilkuaev@gmail.com>
2022-01-20 17:26:11 +0300

    Создали интерфейс регистрации

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -3,15 +3,26 @@
 //  MusicPlayer
 
 //  Created by IosDeveloper on 18.01.2022.
-//
+//𝗽
 
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var label:UILabel = {
+    // Title label
+    let musicPlayerLabel:UILabel = {
         var label = UILabel(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
-        label.text = "Music Player"
+        label.text = "🎧𝙈usic 𝙋layer🎧"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.italicSystemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    //2 LoginLabel
+    let labelLogin:UILabel = {
+        var label = UILabel(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+        label.text = "Login"
         label.textColor = .black
         label.textAlignment = .center
         label.font = UIFont.italicSystemFont(ofSize: 20)
@@ -19,29 +30,31 @@ class ViewController: UIViewController {
         return label
     }()
     
-    var textField1:UITextField = {
+    //Ввод Email
+    let textField1:UITextField = {
         var text = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
-        text.placeholder = "  Input in login"
-        text.textColor = .black
+        text.placeholder = " Enter email"
         text.font = UIFont.italicSystemFont(ofSize: 17)
         text.backgroundColor = .white
-        text.layer.cornerRadius = 15
+        text.layer.cornerRadius = 10
+        text.layer.borderWidth = 1
         text.translatesAutoresizingMaskIntoConstraints = false
         return text
     }()
     
-    var textField2:UITextField = {
+    //Ввод пароля
+    let textField2:UITextField = {
         var text = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
-        text.placeholder = "  Input in password"
-        text.textColor = .black
+        text.placeholder = " Enter password"
         text.backgroundColor = .white
         text.font = UIFont.italicSystemFont(ofSize: 17)
-        text.layer.cornerRadius = 15
-        text.textAlignment = .justified
+        text.layer.cornerRadius = 10
+        text.layer.borderWidth = 1
         text.translatesAutoresizingMaskIntoConstraints = false
         return text
     }()
     
+    //1 кнопка для Входа
     let signInButton:UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
@@ -49,9 +62,12 @@ class ViewController: UIViewController {
         button.tintColor = .black
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
         button.layer.cornerRadius = 15
+//        button.addTarget(self, action: Selector, for: <#T##UIControl.Event#>)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
+    
+    //2 кнопка для Регистрации
     let signUpButton:UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
@@ -59,6 +75,7 @@ class ViewController: UIViewController {
         button.tintColor = .white
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
         button.layer.cornerRadius = 15
+        button.addTarget(self, action: #selector(goToSceneWithSignInButtonTapped), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -70,44 +87,70 @@ class ViewController: UIViewController {
         setupConstrains()
     }
     
+    //Вьюшки
     private func setupViews(){
         view.backgroundColor = #colorLiteral(red: 0, green: 0.9914394021, blue: 1, alpha: 1)
+        //views
         view.addSubview(signInButton)
         view.addSubview(signUpButton)
-        view.addSubview(label)
         view.addSubview(textField1)
         view.addSubview(textField2)
+        //labels
+        view.addSubview(musicPlayerLabel)
+        view.addSubview(labelLogin)
     }
+    //selector для кнопки входа в приложение после ввода логина и пароля
+    @objc private func goToSceneWithSignInButtonTapped(){
+        let registrationViewController = RegistrationViewController()
+        navigationController?.pushViewController(registrationViewController, animated: true)
+        
+    }
+    //selector для кнопки регистрации
+
+    
+    //Констрейнты
     private func setupConstrains(){
+        //1 кнопка signInButton
         NSLayoutConstraint.activate([
             signInButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -200),
             signInButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -100),
             signInButton.heightAnchor.constraint(equalToConstant: 60),
             signInButton.widthAnchor.constraint(equalToConstant: 100)
         ])
+        //2 кнопка signUpButton
         NSLayoutConstraint.activate([
             signUpButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -200),
             signUpButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
             signUpButton.heightAnchor.constraint(equalToConstant: 60),
             signUpButton.widthAnchor.constraint(equalToConstant: 100)
         ])
+        //label Music Player(1)
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
-            label.heightAnchor.constraint(equalToConstant: 50),
-            label.widthAnchor.constraint(equalToConstant: 200)
+            musicPlayerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            musicPlayerLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            musicPlayerLabel.heightAnchor.constraint(equalToConstant: 50),
+            musicPlayerLabel.widthAnchor.constraint(equalToConstant: 200)
         ])
+        //label Login(2)
         NSLayoutConstraint.activate([
-            textField1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 70),
+            labelLogin.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            labelLogin.topAnchor.constraint(equalTo: view.topAnchor, constant: 350),
+            labelLogin.heightAnchor.constraint(equalToConstant: 50),
+            labelLogin.widthAnchor.constraint(equalToConstant: 200)
+        ])
+        // textField(1)
+        NSLayoutConstraint.activate([
+            textField1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             textField1.topAnchor.constraint(equalTo: view.topAnchor,constant: 400),
             textField1.heightAnchor.constraint(equalToConstant: 50),
-            textField1.widthAnchor.constraint(equalToConstant: 300)
+            textField1.widthAnchor.constraint(equalToConstant: 350)
         ])
+        // textField(2)
         NSLayoutConstraint.activate([
-            textField2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 70),
+            textField2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             textField2.topAnchor.constraint(equalTo: view.bottomAnchor,constant: -420),
             textField2.heightAnchor.constraint(equalToConstant: 50),
-            textField2.widthAnchor.constraint(equalToConstant: 300)
+            textField2.widthAnchor.constraint(equalToConstant: 350)
         ])
     }
 }
